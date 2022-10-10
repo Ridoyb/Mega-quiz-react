1e47c4937a9d4c8b012552783e7f2df4b8b05c94
Ridoyb <ridoyb8730@gmail.com>
2022-10-11 02:59:39 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Image from './cover-image.png'
+import './Home.css'
+
 
 const Home = () => {
     return (
         <div>
-            <div>
+            <div className='header-info'>
                 <div>
-                    <img src="" alt="" />
+                    <img className='cover-image' src={Image} alt="" />
+                </div>
+                <div className='cover-text'>
+                    <h4>Mega Quiz is a web-based Audience Engagement Cloud Platform for hosting interactive trivia quizzes at in-person, virtual, and hybrid events. No app install is required</h4>
+                    <h1>Test your brain and skills. <br></br>Are you ready for a quiz?? </h1>
                 </div>
             </div>
         </div>
