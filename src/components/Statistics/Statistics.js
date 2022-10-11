61d950f6e9b3e99eda4e7aa6bc7bab6fec0a98f5
Ridoyb <ridoyb8730@gmail.com>
2022-10-12 03:49:26 +0600

    Chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Line,LineChart } from 'recharts';
+import './Statistics.css'
+
+
 
 const Statistics = () => {
+
+    const data= useLoaderData()
+    console.log(data.data)
+    const [{name,total}]= data.data;
+    console.log(name,total)
+    
+    
+ 
+       
     return (
         <div>
-            <h1>Statistics</h1>
+            <h1 className='stat'>Statistics</h1>
+        <div className='chart'>
+        <ResponsiveContainer width={600} height={600}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data.data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="total" stroke="#8A2BE2" activeDot={{ r: 8 }} />
+          
+        </LineChart>
+      </ResponsiveContainer>
+        </div>
         </div>
     );
 };
